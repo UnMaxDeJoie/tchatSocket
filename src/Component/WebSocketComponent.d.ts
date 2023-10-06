7d2eeceb2be18ce7853c8a6524930fd0932a6747
UnMaxDeJoie <maxait93@gmail.com>
2023-10-06 16:42:50 +0200

    feat(tchatSocker): mise en place de l'application

--- src/Component/WebSocketComponent.d.ts
+++ src/Component/WebSocketComponent.d.ts
@@ -2,6 +2,5 @@ declare module './Component/WebSocketComponent' {
     import React from 'react';
 
     export default class WebSocketComponent extends React.Component {
-        // Déclarez ici les types des propriétés et des états si nécessaire
     }
 }
--- src/Component/WebSocketComponent.jsx
+++ src/Component/WebSocketComponent.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import WebSocketClient from 'websocket';
 
 class WebSocketComponent extends Component {
     constructor(props) {
@@ -7,58 +6,80 @@ class WebSocketComponent extends Component {
         this.state = {
             messages: [],
             message: '',
+            username: '',
         };
-        this.socket = new WebSocketClient('ws://localhost:5173/');
+        this.socket = new WebSocket('ws://localhost:5173');
     }
 
     componentDidMount() {
         this.socket.onopen = () => {
-            console.log('WebSocket connected');
+            console.log('Connexion WebSocket établie.');
         };
 
         this.socket.onmessage = (event) => {
-            const newMessage = JSON.parse(event.data);
+            const receivedMessage = JSON.parse(event.data);
             this.setState((prevState) => ({
-                messages: [...prevState.messages, newMessage],
+                messages: [...prevState.messages, receivedMessage],
             }));
         };
 
+        this.socket.onerror = (error) => {
+            console.error('Une erreur WebSocket s\'est produite :', error);
+        };
+
         this.socket.onclose = () => {
-            console.log('WebSocket closed');
+            console.log('Connexion WebSocket fermée.');
         };
     }
 
+    componentWillUnmount() {
+        this.socket.close();
+    }
+
     sendMessage = () => {
-        const { message } = this.state;
-        const newMessage = {
-            text: message,
-            // Vous pouvez également inclure d'autres informations comme l'expéditeur, la date, etc.
-        };
-        this.socket.send(JSON.stringify(newMessage));
-        this.setState({ message: '' });
+        const { message, username } = this.state;
+        if (message.trim() !== '' && username.trim() !== '') {
+            this.socket.send(JSON.stringify({ text: message, username }));
+            this.setState({ message: '' });
+        }
     };
 
     handleChange = (e) => {
         this.setState({ message: e.target.value });
     };
 
+    handleUsernameChange = (e) => {
+        this.setState({ username: e.target.value });
+    };
+
     render() {
-        const { messages, message } = this.state;
+        const { messages, message, username } = this.state;
 
         return (
             <div>
                 <div>
                     {messages.map((msg, index) => (
-                        <div key={index}>{msg.text}</div>
+                        <div key={index}>
+                            <strong>{msg.username}: </strong>
+                            {msg.text}
+                        </div>
                     ))}
                 </div>
-                <input
-                    type="text"
-                    value={message}
-                    onChange={this.handleChange}
-                    placeholder="Entrez votre message..."
-                />
-                <button onClick={this.sendMessage}>Envoyer</button>
+                <div>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={this.handleUsernameChange}
+                        placeholder="Entrez votre nom d'utilisateur..."
+                    />
+                    <input
+                        type="text"
+                        value={message}
+                        onChange={this.handleChange}
+                        placeholder="Entrez votre message..."
+                    />
+                    <button onClick={this.sendMessage}>Envoyer</button>
+                </div>
             </div>
         );
     }
